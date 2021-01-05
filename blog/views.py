b0a368a43295771dc6c5acbe8ca9e41a0c3ab052
jayanth.s <jayanthsibbala123@gmail.com>
2021-01-05 22:00:58 +0530

    deploy

--- blog/views.py
+++ blog/views.py
@@ -19,8 +19,27 @@ class PostListView(ListView):
 #    return render(request,'blog/post/list.html',{'posts': posts})
 
 #def post_detail(request, year, month, day, post):
-#    post = get_object_or_404(Post, slug=post,status='published',publish_year=year,publishmonth=month,publish_day=day)
+#    post = get_object_or_404(Post, slug=post,status='published',publish__year=year,publish__month=month,publish__day=day)
 #    return render(request,'blog/post/detail.html',{'post': post})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,status='published',publish__year=year,publish__month=month,publish__day=day)
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+    # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+    # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+    # Assign the current post to the comment
+            new_comment.post = post
+    # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request,'blog/post/detail.html',{'post': post,'comments': comments,'new_comment': new_comment,'comment_form': comment_form})
 
 def post_list(request):
     object_list = Post.published.all()
@@ -60,22 +79,3 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
         return render(request, 'blog/post/share.html', {'post':post,'form':form})
-
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post,status='published',publish__year=year,publish__month=month,publish__day=day)
-    # List of active comments for this post
-    comments = post.comments.filter(active=True)
-    new_comment = None
-    if request.method == 'POST':
-    # A comment was posted
-        comment_form = CommentForm(data=request.POST)
-    if comment_form.is_valid():
-    # Create Comment object but don't save to database yet
-        new_comment = comment_form.save(commit=False)
-    # Assign the current post to the comment
-        new_comment.post = post
-    # Save the comment to the database
-        new_comment.save()
-    else:
-        comment_form = CommentForm()
-    return render(request,'blog/post/detail.html',{'post': post,'comments': comments,'new_comment': new_comment,'comment_form': comment_form})
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+git clone https://github.com/vitorfs/bootcamp.git && cd bootcamp
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
+
